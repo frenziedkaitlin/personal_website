f39022424a08ab3faa49c92c1e26f681fee1846a
kaitlin <kaitlin.menzie@gmail.com>
2017-10-02 14:27:20 -0400

    https

--- scripts/googlesheets.js
+++ scripts/googlesheets.js
@@ -1,9 +1,30 @@
-var url ='https://spreadsheets.google.com/feeds/list/PUB_ID/values?alt=json-in-script&callback=x';
-var id = '2PACX-1vSNtn3oG0TMMy4Manxu-y6tMzoDGp2JlNBTIzNmjnoadrl_VjaFlr_fw2DifD88W5EMo085lxVBdq7-'
+var url ='https://docs.google.com/spreadsheets/d/e/PUB_ID/pub?gid=0&single=true&output=csv';
+var id = '2PACX-1vSNtn3oG0TMMy4Manxu-y6tMzoDGp2JlNBTIzNmjnoadrl_VjaFlr_fw2DifD88W5EMo085lxVBdq7-';
 
 $(function() {
 	url = url.replace('PUB_ID', id);
-	$.get(url, function(data){
+	var test = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSNtn3oG0TMMy4Manxu-y6tMzoDGp2JlNBTIzNmjnoadrl_VjaFlr_fw2DifD88W5EMo085lxVBdq7-/pub?gid=0&single=true&output=csv'
+	$.get(test)
+	.then(csv_as_object)
+	.then(function(data){
 		console.log(data);
 	});
 });
+
+function csv_as_object(data){
+	return new Promise(function(resolve, reject){
+		var obj = {};
+		var rows = data.split("\n");
+		for (r in rows){
+			var columns = rows[r].split(",");
+			var identifier = columns[0];
+			obj[identifier] = [];
+			if(columns.length > 1){
+				for(var i = 1; i < columns.length; i++){
+					obj[identifier].push(columns[i]);
+				}
+			}
+		}
+		resolve(obj);
+	});
+}
