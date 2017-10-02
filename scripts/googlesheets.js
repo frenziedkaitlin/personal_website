fb43e2b0028ee06a7c1e14e7c33009e5565f6dcd
kaitlin <kaitlin.menzie@gmail.com>
2017-10-02 13:45:47 -0400

    switch to jsonp

--- scripts/googlesheets.js
+++ scripts/googlesheets.js
@@ -1,3 +1,11 @@
-$.get('https://docs.google.com/spreadsheets/d/e/2PACX-1vSNtn3oG0TMMy4Manxu-y6tMzoDGp2JlNBTIzNmjnoadrl_VjaFlr_fw2DifD88W5EMo085lxVBdq7-/pub?gid=0&single=true&output=csv', function(data){
-	console.log(data);
-})
+var url ='https://spreadsheets.google.com/feeds/list/PUB_ID/values?alt=json-in-script&callback=x';
+var id = '2PACX-1vSNtn3oG0TMMy4Manxu-y6tMzoDGp2JlNBTIzNmjnoadrl_VjaFlr_fw2DifD88W5EMo085lxVBdq7-'
+
+
+$(
+	url = url.replace('PUB_ID', id);
+	$.get(url, function(data){
+
+		console.log(data);
+	})
+)
