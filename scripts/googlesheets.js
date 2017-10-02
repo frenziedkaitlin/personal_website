0f063b09179251fd14f78ec005460c411a633025
kaitlin <kaitlin.menzie@gmail.com>
2017-10-02 13:38:58 -0400

    moved scripts out

---
+++ scripts/googlesheets.js
@@ -0,0 +1,3 @@
+$.get('https://docs.google.com/spreadsheets/d/e/2PACX-1vSNtn3oG0TMMy4Manxu-y6tMzoDGp2JlNBTIzNmjnoadrl_VjaFlr_fw2DifD88W5EMo085lxVBdq7-/pub?gid=0&single=true&output=csv', function(data){
+	console.log(data);
+})
