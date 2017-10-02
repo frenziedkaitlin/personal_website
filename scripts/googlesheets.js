8f6c2355aa56e47b75308e2644223e7627fcd490
kaitlin <kaitlin.menzie@gmail.com>
2017-10-02 13:47:55 -0400

    switch to jsonp

--- scripts/googlesheets.js
+++ scripts/googlesheets.js
@@ -1,11 +1,9 @@
 var url ='https://spreadsheets.google.com/feeds/list/PUB_ID/values?alt=json-in-script&callback=x';
 var id = '2PACX-1vSNtn3oG0TMMy4Manxu-y6tMzoDGp2JlNBTIzNmjnoadrl_VjaFlr_fw2DifD88W5EMo085lxVBdq7-'
 
-
-$(
+$(function() {
 	url = url.replace('PUB_ID', id);
 	$.get(url, function(data){
-
 		console.log(data);
-	})
-)
+	});
+});
